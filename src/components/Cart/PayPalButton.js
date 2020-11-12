022ad5e6da3edb95fadda861fde4177dbded0fe5
firshta2016 <firshtashefa@gmail.com>
2020-11-12 14:22:30 -0800

    changed paypal config key

--- src/components/Cart/PayPalButton.js
+++ src/components/Cart/PayPalButton.js
@@ -31,7 +31,7 @@ export default class MyApp extends React.Component {
  
         const client = {
             sandbox: process.env.PAYPAL_KEY,
-            production: 'YOUR-PRODUCTION-APP-ID',
+            production: process.env.PAYPAL_KEY,
         }
         // In order to get production's app-ID, you will have to send your app to Paypal for approval first
         // For sandbox app-ID (after logging into your developer account, please locate the "REST API apps" section, click "Create App"):

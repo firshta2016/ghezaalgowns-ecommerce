bed25744ddfd1848a834c87832de1532a9e839df
firshta2016 <firshtashefa@gmail.com>
2021-03-04 15:21:30 -0800

    updated the nav bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,44 +7,49 @@ import {ButtonContainer} from './Button';
 export default class Navbar extends Component {
     render() {
         return (
-            <NavWrapper className="navbar navbar-expand-md navbar-dark px-sm-5">
-                <Link to="/">
+            <NavWrapper>
+                <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                    <div class="container-fluid">
+                    <Link to="/">
                     <img src={logo} alt="logo" className='logo navbar-brand' />
-                    A Bridal Gown Shop
-                </Link>
-            {/** Toggler/collapsibel Button*/}
-                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target='#collapsibleNavbar' >
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-            {/**Navbar Links */}
-                <div className="collapse navbar-collapse" id="collapsibleNavbar">
-                <ul className="navbar-nav">
-                <li className="active nav-item">
-                    <Link to="/" className="nav-link">
-                        bride 
+                        A Bridal Gown Shop
                     </Link>
-                </li>
-                <li className="active nav-item">
-                    <Link to="BridesMaid" className="nav-link">
-                        bridesmaid & bridal party 
-                    </Link>
-                </li>
-                <li className="active nav-item">
-                    <Link to="/Shoes" className="nav-link">
-                        shoes & accessories
-                    </Link>
-                </li>
-
-                <Link to="/Cart" className="ml-auto">
-                    <ButtonContainer>
-                    <span>
-                        <i className="fa fa-cart-plus" />
-                    </span>
-                    my cart
-                    </ButtonContainer>
-                </Link>
-                </ul>
+                        <button
+                        class="navbar-toggler"
+                        type="button"
+                        data-mdb-toggle="collapse"
+                        data-mdb-target="#navbarRightAlignExample"
+                        aria-controls="navbarRightAlignExample"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                        >
+                        <i class="fas fa-bars"></i>
+                        </button>
+                    <div class="collapse navbar-collapse" id="navbarRightAlignExample">
+                        <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                            <Link to="/" className="nav-link"> bride </Link>
+                            </li>
+                            <li class="nav-item">
+                            <Link to="BridesMaid" className="nav-link">bridesmaid</Link>
+                            </li>
+                            <li className="active nav-item">
+                            <Link to="/Shoes" className="nav-link">
+                                 shoes
+                              </Link>
+                          </li>
+                            <Link to="/Cart" className="ml-auto">
+                                <ButtonContainer>
+                                <span>
+                                    <i className="fa fa-cart-plus" />
+                                </span>
+                                my cart
+                                </ButtonContainer>
+                            </Link>
+                        </ul>
+                    </div>
                 </div>
+                </nav>
             </NavWrapper>
         )
     }
